3f0d2339ab8eb9100b098c66cda5be45b131e72f
minsigyyy <minsigyyy@g.shingu.ac.kr>
2023-09-20 15:09:52 +0900

    week04 v20

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -10,5 +10,23 @@ public class PokemonGame {
 	at Pokemon.performFly(Pokemon.java:18)
 	at PokemonGame.main(PokemonGame.java:5)
          */
+        System.out.println("포켓몬 게임을 시작합니다\n야생 포켓몬이 나타났습니다");
+//        System.out.println(Math.random());  // 0.0 <= x < 1.0
+//        System.out.println((int)(Math.random()*6)+1);  // 1 <= x <= 6
+
+        // 적군 포켓몬스터 랜덤 생성
+        int enemyPick = (int)(Math.random()*3);
+        if(enemyPick == 0){
+            NoFly NoFly = new NoFly();
+            Pikachu Pikachu = new Pikachu(noFly);
+        }else if(enemyPick == 1){
+            NoFly NoFly = new NoFly();
+            Squirtle squirtle = new Squirtle(noFly);
+        }else if(enemyPick == 2){
+            Wings wings = new Wings();
+            Charizard charizard = new Charizard(wings);
+        }else{
+            System.out.println("여기는 영원히 실행 안됩니다");
+        }
     }
 }
