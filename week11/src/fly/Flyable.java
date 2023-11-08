5586f63865732c79bf420fc553c93de2920a63ef
minsigyyy <minsigyyy@g.shingu.ac.kr>
2023-11-08 15:15:20 +0900

    week11 v49

---
+++ week11/src/fly/Flyable.java
@@ -0,0 +1,17 @@
+package fly;
+public interface Flyable {
+//    public fly.Flyable(){
+//
+//    }
+
+    //Pokemonster.Pokemon pokemon;
+
+    //void fly();
+    // default void fly(){
+  //      System.out.println("날아 갑니다!");
+  //  }
+    void fly();  // 추상 메서드
+//    default void fly(){  // 구현 코드가 있는 디폴트 메서드
+//        System.out.println("날아 갑니다!");
+//    }
+}
---
+++ week11/src/fly/Wings.java
@@ -0,0 +1,15 @@
+package fly;
+import fly.Flyable;
+
+public class Wings implements Flyable {
+    //    @Override
+//    public void fly() {
+//
+//        System.out.println("날개를 펼쳐 날아갑니다~~");
+//    }
+    @Override
+    public void fly() {
+
+        System.out.println("날개를 펼쳐 날아갑니다~~");
+    }
+}
